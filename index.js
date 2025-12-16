0af5ea7d1569c77e4ad76dc111b28e23167f7cf1
Jopila <marcelomarangoni767@gmail.com>
2025-12-16 11:55:23 -0300

    create production db

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-const NotFoundError = require('./errors/not-found-error');
-const handleErrors = require('./middlewares/error-handler');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const NotFoundError = require("./errors/not-found-error");
+const handleErrors = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const {
   PORT = 3000,
-  MONGO_URI = 'mongodb://127.0.0.1:27017/news_explorer',
+  MONGO_URI = "mongodb+srv://marcelomarangoni767_db_user:d7rqy45H6ExkoFYk@cluster0.prse0q9.mongodb.net/news-eplorer?appName=Cluster0",
 } = process.env;
 
 const app = express();
@@ -19,7 +19,9 @@ app.use(requestLogger);
 mongoose.connect(MONGO_URI);
 
 app.use(routes);
-app.use('*', (req, res, next) => next(new NotFoundError('Rota não encontrada')));
+app.use("*", (req, res, next) =>
+  next(new NotFoundError("Rota não encontrada"))
+);
 app.use(errorLogger);
 app.use(handleErrors);
 
