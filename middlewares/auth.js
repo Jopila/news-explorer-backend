33f8b47c208ff9b9389eb2c559918e696430b114
Jopila <marcelomarangoni767@gmail.com>
2025-12-16 11:29:23 -0300

    check: node .env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,27 @@
-const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/unauthorized-error');
-
-const { JWT_SECRET = 'dev-secret' } = process.env;
-
+const jwt = require("jsonwebtoken");
+const UnauthorizedError = require("../errors/unauthorized-error");
+let JWT_SECRET;
+if (process.env.NODE_ENV !== "production") {
+  // Código para desenvolvimento
+  JWT_SECRET = "dev-secret";
+} else {
+  // Código para produção
+  JWT_SECRET = process.env.JWT_SECRET;
+}
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Autorização necessária'));
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Autorização necessária"));
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace("Bearer ", "");
 
   try {
     const payload = jwt.verify(token, JWT_SECRET);
     req.user = payload;
     return next();
   } catch (err) {
-    return next(new UnauthorizedError('Token inválido'));
+    return next(new UnauthorizedError("Token inválido"));
   }
 };
